ce62c86d07ec12dbeb927a86ac74de4d71f5d7ae
JSH-data <super60447@gmail.com>
2021-09-27 20:09:34 +0900

    test

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withImages = require('next-images');
 module.exports = withImages();
 
 module.exports = {
-  assetPrefix: isProd ? 'https://cdn.mydomain.com' : '',
+  assetPrefix: isProd ? 'https://ctrlf.space' : '',
   env: {
     MOCK_PUBLIC_BASE_API: process.env.MOCK_PUBLIC_BASE_API,
     PUBLIC_BASE_API: process.env.PUBLIC_BASE_API,
