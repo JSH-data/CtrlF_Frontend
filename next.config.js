79f6608da9b837c928ead34ccb104b930807444e
JSH-data <super60447@gmail.com>
2021-09-28 17:14:06 +0900

    plz

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-const isProd = process.env.NODE_ENV === 'production';
 const withImages = require('next-images');
 module.exports = withImages();
 
 module.exports = {
-  assetPrefix: isProd ? 'https://ctrlf.space' : '',
   env: {
     MOCK_PUBLIC_BASE_API: process.env.MOCK_PUBLIC_BASE_API,
     PUBLIC_BASE_API: process.env.PUBLIC_BASE_API,
